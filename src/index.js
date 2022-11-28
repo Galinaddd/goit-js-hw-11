074e9b6d13b725dbcf47b9717c0b5a31fde77984
Galina <galkozavrik@ukr.net>
2022-11-28 23:28:54 +0300

    реалізує завдання дз

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-var lightbox = new SimpleLightbox('.gallery a');
+const lightbox = new SimpleLightbox('.gallery a');
+
 import axios from 'axios';
 
 const formRef = document.querySelector('.search-form');
@@ -12,18 +13,27 @@ const loadBtnRef = document.querySelector('.load-more');
 const galleryRef = document.querySelector('.gallery');
 
 formRef.addEventListener('submit', onSearch);
+loadBtnRef.addEventListener('click', onLoadMore);
+
+let page = 1;
+let keyword = '';
 
 async function onSearch(evt) {
+  page = 1;
   evt.preventDefault();
-  console.log('its a search');
-  const keyword = evt.currentTarget.elements.searchQuery.value.trim();
+  console.log('its a search', page);
+
+  keyword = evt.currentTarget.elements.searchQuery.value.trim();
 
   const resault = await searchByKeyword(keyword);
+  console.log('resault', resault);
 
   if (resault.data.hits.length) {
     galleryRef.innerHTML = createMarkup(resault.data.hits);
+    lightbox.refresh();
     loadBtnRef.removeAttribute('hidden');
-    // loadBtnRef.addEventListener('click', onLoadMore);
+
+    console.log(loadBtnRef);
   } else
     Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
@@ -33,10 +43,10 @@ async function onSearch(evt) {
 async function searchByKeyword(keyword) {
   const BASE_URL = 'https://pixabay.com/api/';
   const KEY = 'key=31640971-742b32395d0b897b4125e0e70';
+  const PER_PAGE = 40;
   const params = `image_type=photo&orientation=horizontal&safesearch=true`;
-
-  const searchQuery = `${BASE_URL}?${KEY}&${params}&q=${keyword}`;
-  console.log(searchQuery);
+  const searchQuery = `${BASE_URL}?${KEY}&${params}&per_page=${PER_PAGE}&page=${page}&q=${keyword}`;
+  // console.log(searchQuery);
   return (response = await axios.get(searchQuery));
 }
 
@@ -67,4 +77,16 @@ function createMarkup(arr) {
     .join(' ');
 }
 
-// function onLoadMore() {}
+async function onLoadMore() {
+  page += 1;
+  console.log('load more click');
+  const resault = await searchByKeyword(keyword);
+  console.log(resault);
+  galleryRef.insertAdjacentHTML('beforeend', createMarkup(resault.data.hits));
+  if (page * 40 >= resault.data.totalHits) {
+    loadBtnRef.setAttribute('hidden', true);
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+}
